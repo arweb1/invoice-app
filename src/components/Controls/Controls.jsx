d1ae64b488563c05e07ec305abbc33fb6ed8ba1d
arweb1 <agaevrweb@gmail.com>
2023-10-31 20:21:22 +0200

    checkbox styled

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -19,60 +19,71 @@ function Controls() {
   }
 
   const handleFilter = (filterCriteria) => {
-    if(!activeFilters.includes(filterCriteria)){
+    if (!activeFilters.includes(filterCriteria)) {
       dispatch(addFilter(filterCriteria))
-    }else{
+    } else {
       dispatch(removeFilter(filterCriteria))
     }
   }
-  
+
   return (
     <div className="controls">
-        <div className="controls-leftside text-content">
-            <h2 className='controls-leftside__tag'>Invoices</h2>
-            <p className="invoices-counter">There are {invoiceList.length} total invoices</p>
-        </div>
-        <div className="controls-roghtside">
-            <div className="filter-container">
-              <div 
-                onClick={() => handleShowFilters()}
-                className={`filter-btn`}
-                >
-                <p>Filter by status</p>
-                <img 
-                  src={arrow} 
-                  alt="arrow down"
-                  style={{transform: isFiltersVisible ? 'rotate(180deg)' : ''}}
+      <div className="controls-leftside text-content">
+        <h2 className='controls-leftside__tag'>Invoices</h2>
+        <p className="invoices-counter">There are {invoiceList.length} total invoices</p>
+      </div>
+      <div className="controls-roghtside">
+        <div className="filter-container">
+          <div
+            onClick={() => handleShowFilters()}
+            className={`filter-btn`}
+          >
+            <p>Filter by status</p>
+            <img
+              src={arrow}
+              alt="arrow down"
+              style={{ transform: isFiltersVisible ? 'rotate(180deg)' : '' }}
+            />
+          </div>
+          <div className={`filter-content ${isFiltersVisible ? 'active' : ''}`}>
+            <div className="filter-content-filters">
+              <div className="filter-item" >
+                <input
+                  type="checkbox"
+                  id="filterDraft"
+                  checked={activeFilters.includes('Draft')}
+                  onChange={() => handleFilter('Draft')} // Добавьте обработчик изменения
                 />
+                <label htmlFor="filterDraft">Draft</label>
               </div>
-              <div className={`filter-content ${isFiltersVisible ? 'active' : ''}`}>
-                <div className="filter-content-filters">
-                  <div className="filter-item" onClick={() => handleFilter('Draft')}>
-                    <input type="checkbox" />
-                    <label htmlFor="">Draft</label>
-                  </div>
-                  <div className="filter-item" onClick={() => handleFilter('Pending')}>
-                    <input type="checkbox" />
-                    <label htmlFor="">Pending</label>
-                  </div>
-                  <div className="filter-item" onClick={() => handleFilter('Paid')}>
-                    <input type="checkbox" />
-                    <label htmlFor="">Paid</label>
-                  </div>
-                </div>
+              <div className="filter-item">
+                <input
+                  type="checkbox"
+                  id="filterPending"
+                  checked={activeFilters.includes('Pending')}
+                  onChange={() => handleFilter('Pending')}
+                />
+                <label htmlFor="filterPending">Pending</label>
+              </div>
+              <div className="filter-item">
+                <input
+                  type="checkbox"
+                  id="filterPaid"
+                  checked={activeFilters.includes('Paid')}
+                  onChange={() => handleFilter('Paid')}
+                />
+                <label htmlFor="filterPaid">Paid</label>
               </div>
             </div>
-            <button className="new-invoice btn1" onClick={() => dispatch(showForm())}>
-                <img src={plusImage} alt="plus" />
-                <p>New Invoice</p>
-            </button>
+          </div>
         </div>
+        <button className="new-invoice btn1" onClick={() => dispatch(showForm())}>
+          <img src={plusImage} alt="plus" />
+          <p>New Invoice</p>
+        </button>
+      </div>
     </div>
   )
 }
 
 export default Controls;
-
-
-
-
